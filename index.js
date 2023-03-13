72b698f2053bad73eb212a06a031fb78ff6e6235
+ <+>
2023-03-14 00:57:28 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+let multiply
+
+let num1 = 31
+let num2 = 2
+
+multiply = num1 * num2
+
+console.log(multiply);
+
+//random integers
+let  random
+
+random = Math.ceil(Math.random())
+
+
+//modulus
+let num3 = 24
+let num4 = 5;
+let mod = (num3%num4);
+
+//max
+
+const max = Math.max(10,20);
+
+
